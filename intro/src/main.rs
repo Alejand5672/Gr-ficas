1f3326cf0bf58b481c720945f6cd8fb7c1ea932b
Alejand5672 <her241424@uvg.edu.gt>
2026-07-11 19:38:23 -0600

    Ejercicio de introduccion

---
+++ intro/src/main.rs
@@ -0,0 +1,183 @@
+use raylib::prelude::*;
+
+fn main() {
+    let image_width = 500;
+    let image_height = 500;
+
+    let mut new_image = Image::gen_image_color(
+        image_width,
+        image_height,
+        Color::BLACK,
+    );
+
+    
+    new_image.draw_pixel(100, 100, Color::WHITE);
+    new_image.draw_pixel(101, 101, Color::WHITE);
+    new_image.draw_pixel(101, 100, Color::WHITE);
+    new_image.draw_pixel(100, 101, Color::WHITE);
+    new_image.draw_pixel(110, 100, Color::WHITE);
+    new_image.draw_pixel(111, 100, Color::WHITE);
+    new_image.draw_pixel(110, 101, Color::WHITE);
+    new_image.draw_pixel(111, 101, Color::WHITE);
+    new_image.draw_pixel(95, 106, Color::WHITE);
+    new_image.draw_pixel(96, 106, Color::WHITE);
+    new_image.draw_pixel(97, 106, Color::WHITE);
+    new_image.draw_pixel(98, 106, Color::WHITE);
+    new_image.draw_pixel(99, 106, Color::WHITE);
+    new_image.draw_pixel(100, 106, Color::WHITE);
+    new_image.draw_pixel(101, 106, Color::WHITE);
+    new_image.draw_pixel(102, 106, Color::WHITE);
+    new_image.draw_pixel(103, 106, Color::WHITE);
+    new_image.draw_pixel(104, 106, Color::WHITE);
+    new_image.draw_pixel(105, 106, Color::WHITE);
+    new_image.draw_pixel(106, 106, Color::WHITE);
+    new_image.draw_pixel(107, 106, Color::WHITE);
+    new_image.draw_pixel(108, 106, Color::WHITE);
+    new_image.draw_pixel(109, 106, Color::WHITE);
+    new_image.draw_pixel(110, 106, Color::WHITE);
+    new_image.draw_pixel(111, 106, Color::WHITE);
+    new_image.draw_pixel(112, 106, Color::WHITE);
+    new_image.draw_pixel(113, 106, Color::WHITE);
+    new_image.draw_pixel(114, 106, Color::WHITE);
+    new_image.draw_pixel(115, 106, Color::WHITE);
+    new_image.draw_pixel(116, 106, Color::WHITE);
+    new_image.draw_pixel(116, 107, Color::WHITE);
+    new_image.draw_pixel(116, 108, Color::WHITE);
+    new_image.draw_pixel(116, 109, Color::WHITE);
+    new_image.draw_pixel(116, 110, Color::WHITE);
+    new_image.draw_pixel(116, 111, Color::WHITE);
+    new_image.draw_pixel(116, 112, Color::WHITE);
+    new_image.draw_pixel(116, 113, Color::WHITE);
+    new_image.draw_pixel(116, 114, Color::WHITE);
+    new_image.draw_pixel(116, 115, Color::WHITE);
+    new_image.draw_pixel(116, 116, Color::WHITE);
+    new_image.draw_pixel(95, 107, Color::WHITE);
+    new_image.draw_pixel(95, 108, Color::WHITE);
+    new_image.draw_pixel(95, 109, Color::WHITE);
+    new_image.draw_pixel(95, 110, Color::WHITE);
+    new_image.draw_pixel(95, 111, Color::WHITE);
+    new_image.draw_pixel(95, 112, Color::WHITE);
+    new_image.draw_pixel(95, 113, Color::WHITE);
+    new_image.draw_pixel(95, 114, Color::WHITE);
+    new_image.draw_pixel(95, 115, Color::WHITE);
+    new_image.draw_pixel(95, 116, Color::WHITE);
+
+    new_image.draw_pixel(95, 116, Color::WHITE);
+    new_image.draw_pixel(96, 116, Color::WHITE);
+    new_image.draw_pixel(97, 116, Color::WHITE);
+    new_image.draw_pixel(98, 116, Color::WHITE);
+    new_image.draw_pixel(99, 116, Color::WHITE);
+    new_image.draw_pixel(100, 116, Color::WHITE);
+    new_image.draw_pixel(101, 116, Color::WHITE);
+    new_image.draw_pixel(102, 116, Color::WHITE);
+    new_image.draw_pixel(103, 116, Color::WHITE);
+    new_image.draw_pixel(104, 116, Color::WHITE);
+    new_image.draw_pixel(105, 116, Color::WHITE);
+    new_image.draw_pixel(106, 116, Color::WHITE);
+    new_image.draw_pixel(107, 116, Color::WHITE);
+    new_image.draw_pixel(108, 116, Color::WHITE);
+    new_image.draw_pixel(109, 116, Color::WHITE);
+    new_image.draw_pixel(110, 116, Color::WHITE);
+    new_image.draw_pixel(111, 116, Color::WHITE);
+    new_image.draw_pixel(112, 116, Color::WHITE);
+    new_image.draw_pixel(113, 116, Color::WHITE);
+    new_image.draw_pixel(114, 116, Color::WHITE);
+    new_image.draw_pixel(115, 116, Color::WHITE);
+    
+
+    new_image.draw_pixel(140, 101, Color::WHITE);
+    new_image.draw_pixel(140, 102, Color::WHITE);
+    new_image.draw_pixel(140, 103, Color::WHITE);
+    new_image.draw_pixel(140, 104, Color::WHITE);
+    new_image.draw_pixel(140, 105, Color::WHITE);
+    new_image.draw_pixel(140, 106, Color::WHITE);
+    new_image.draw_pixel(140, 107, Color::WHITE);
+    new_image.draw_pixel(140, 108, Color::WHITE);
+    new_image.draw_pixel(140, 109, Color::WHITE);
+    new_image.draw_pixel(140, 110, Color::WHITE);
+    new_image.draw_pixel(140, 111, Color::WHITE);
+    new_image.draw_pixel(140, 112, Color::WHITE);
+    new_image.draw_pixel(140, 113, Color::WHITE);
+    new_image.draw_pixel(140, 114, Color::WHITE);
+    new_image.draw_pixel(140, 115, Color::WHITE);
+    new_image.draw_pixel(140, 116, Color::WHITE);
+
+    new_image.draw_pixel(141, 116, Color::WHITE);
+    new_image.draw_pixel(142, 116, Color::WHITE);
+    new_image.draw_pixel(143, 116, Color::WHITE);
+    new_image.draw_pixel(144, 116, Color::WHITE);
+    new_image.draw_pixel(145, 116, Color::WHITE);
+    new_image.draw_pixel(146, 116, Color::WHITE);
+    new_image.draw_pixel(147, 116, Color::WHITE);
+    new_image.draw_pixel(148, 116, Color::WHITE);
+    new_image.draw_pixel(149, 116, Color::WHITE);
+    new_image.draw_pixel(150, 116, Color::WHITE);
+    new_image.draw_pixel(151, 116, Color::WHITE);
+    new_image.draw_pixel(152, 116, Color::WHITE);
+    new_image.draw_pixel(153, 116, Color::WHITE);
+    new_image.draw_pixel(154, 116, Color::WHITE);
+    new_image.draw_pixel(155, 116, Color::WHITE);
+    new_image.draw_pixel(156, 116, Color::WHITE);
+
+    new_image.draw_pixel(156, 101, Color::WHITE);
+    new_image.draw_pixel(156, 102, Color::WHITE);
+    new_image.draw_pixel(156, 103, Color::WHITE);
+    new_image.draw_pixel(156, 104, Color::WHITE);
+    new_image.draw_pixel(156, 105, Color::WHITE);
+    new_image.draw_pixel(156, 106, Color::WHITE);
+    new_image.draw_pixel(156, 107, Color::WHITE);
+    new_image.draw_pixel(156, 108, Color::WHITE);
+    new_image.draw_pixel(156, 109, Color::WHITE);
+    new_image.draw_pixel(156, 110, Color::WHITE);
+    new_image.draw_pixel(156, 111, Color::WHITE);
+    new_image.draw_pixel(156, 112, Color::WHITE);
+    new_image.draw_pixel(156, 113, Color::WHITE);
+    new_image.draw_pixel(156, 114, Color::WHITE);
+    new_image.draw_pixel(156, 115, Color::WHITE);
+
+    new_image.draw_pixel(157, 116, Color::WHITE);
+    new_image.draw_pixel(158, 116, Color::WHITE);
+    new_image.draw_pixel(159, 116, Color::WHITE);
+    new_image.draw_pixel(160, 116, Color::WHITE);
+    new_image.draw_pixel(161, 116, Color::WHITE);
+    new_image.draw_pixel(162, 116, Color::WHITE);
+    new_image.draw_pixel(163, 116, Color::WHITE);
+    new_image.draw_pixel(164, 116, Color::WHITE);
+    new_image.draw_pixel(165, 116, Color::WHITE);
+    new_image.draw_pixel(166, 116, Color::WHITE);
+    new_image.draw_pixel(167, 116, Color::WHITE);
+    new_image.draw_pixel(168, 116, Color::WHITE);
+    new_image.draw_pixel(169, 116, Color::WHITE);
+    new_image.draw_pixel(170, 116, Color::WHITE);
+    new_image.draw_pixel(171, 116, Color::WHITE);
+    new_image.draw_pixel(172, 116, Color::WHITE);
+
+    new_image.draw_pixel(172, 101, Color::WHITE);
+    new_image.draw_pixel(172, 102, Color::WHITE);
+    new_image.draw_pixel(172, 103, Color::WHITE);
+    new_image.draw_pixel(172, 104, Color::WHITE);
+    new_image.draw_pixel(172, 105, Color::WHITE);
+    new_image.draw_pixel(172, 106, Color::WHITE);
+    new_image.draw_pixel(172, 107, Color::WHITE);
+    new_image.draw_pixel(172, 108, Color::WHITE);
+    new_image.draw_pixel(172, 109, Color::WHITE);
+    new_image.draw_pixel(172, 110, Color::WHITE);
+    new_image.draw_pixel(172, 111, Color::WHITE);
+    new_image.draw_pixel(172, 112, Color::WHITE);
+    new_image.draw_pixel(172, 113, Color::WHITE);
+    new_image.draw_pixel(172, 114, Color::WHITE);
+    new_image.draw_pixel(172, 115, Color::WHITE);
+    
+    
+
+
+
+    let output_file_name = "my_first_image.png";
+
+    new_image.export_image(output_file_name);
+
+    println!(
+        "¡Imagen guardada correctamente como '{}'!",
+        output_file_name
+    );
+}
